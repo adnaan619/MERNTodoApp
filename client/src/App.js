06b49ba8f8bc2de3b8af87d4c7c45bd0709219b8
adnaan619 <86790106+adnaan619@users.noreply.github.com>
2022-06-22 14:29:28 -0700

    code changed and also the file structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <div className="row">
+          <form className="col s12">
+            <div className="row">
+              <div className="input-field col s6">
+                <i className="material-icons prefix">account_circle</i>
+                <input id="icon_prefix" type="text" className="validate" />
+                <label htmlFor="icon_prefix">Title</label>
+              </div>
+              <div className="input-field col s6">
+                <i className="material-icons prefix">description</i>
+                <input id="icon_telephone" type="tel" className="validate" />
+                <label htmlFor="icon_telephone">content</label>
+              </div>
+            </div>
+          </form>
+        </div>
+
+    
     </div>
   );
 }
